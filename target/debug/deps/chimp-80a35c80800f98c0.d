8ae762eb25f01922ffe171b6f23c2714d6e05ee6
mo <morzsk@pm.me>
2026-06-10 14:25:55 +0000

    add .gitignore and untrack Rust build artifacts

--- target/debug/deps/chimp-80a35c80800f98c0.d
+++
@@ -1,9 +0,0 @@
-/home/mo/my-drive/my-dotfiles/chimp/target/debug/deps/chimp-80a35c80800f98c0.d: src/main.rs Cargo.toml
-
-/home/mo/my-drive/my-dotfiles/chimp/target/debug/deps/libchimp-80a35c80800f98c0.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- target/debug/deps/chimp-f9c402f68273722b.d
+++
@@ -1,9 +0,0 @@
-/home/mo/my-drive/my-dotfiles/chimp/target/debug/deps/chimp-f9c402f68273722b.d: src/main.rs Cargo.toml
-
-/home/mo/my-drive/my-dotfiles/chimp/target/debug/deps/libchimp-f9c402f68273722b.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
